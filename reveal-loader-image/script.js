13af2e448de57cb8e41fb1010faf7d684e2e4363
prithiviPM <prithivipemimagar@gmail.com>
2026-01-02 15:40:26 +0545

    Refactor HTML and CSS for improved layout and navigation; enhance GSAP animations for image reveal effect

--- reveal-loader-image/script.js
+++ reveal-loader-image/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll(".image");
 
-  const splitText = new SplitText("h1", { type: "words,chars" });
+  const splitText = new SplitText(".text h1", { type: "words,chars" });
 
   images.forEach((img, index) => {
     if (index !== 0) {
@@ -28,10 +28,25 @@ document.addEventListener("DOMContentLoaded", function () {
       transformOrigin: "center top",
       delay: 1,
     })
-    .to(splitText.chars, {
-      y: "0%",
-      duration: 1,
-      ease: "power2.easeOut",
-      stagger: 0.05,
-    });
+    .to(
+      splitText.chars,
+      {
+        y: "100%",
+        duration: 0.6,
+        ease: "power2.easeOut",
+        stagger: 0.05,
+      },
+      "-=0.9"
+    )
+    .from(
+      ".nav",
+      {
+        y: -50,
+        opacity: 0,
+        duration: 0.5,
+        ease: "power2.out",
+        stagger: 0.2,
+      },
+      "-=0.5"
+    );
 });
