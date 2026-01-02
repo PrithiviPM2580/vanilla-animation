7dda0c544b69a2166cd070c739890a5719e4d990
prithiviPM <prithivipemimagar@gmail.com>
2026-01-02 19:54:12 +0545

    Implement hover text feature with GSAP animations and CSS styles

---
+++ hover-text/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll("p");
+
+  elements.forEach((el) => {
+    let innerText = el.innerText;
+    el.innerHTML = "";
+
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("block");
+
+    for (let letter of innerText) {
+      let span = document.createElement("span");
+      span.innerText = letter.trim() === "" ? "\xa0" : letter;
+      span.classList.add("letter");
+      textContainer.appendChild(span);
+    }
+    el.appendChild(textContainer);
+    el.appendChild(textContainer.cloneNode(true));
+  });
+
+  elements.forEach((el) => {
+    el.addEventListener("mouseover", () => {
+      el.classList.remove("play");
+    });
+  });
+});
