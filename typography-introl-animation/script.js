593bcf38a6b0e4f64c5a3dfbb96b1b1b78025268
prithiviPM <prithivipemimagar@gmail.com>
2026-01-03 13:55:08 +0545

    Enhance animation effects and update styles for typography intro animation

--- typography-introl-animation/script.js
+++ typography-introl-animation/script.js
@@ -3,6 +3,58 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector(".btn");
 
   btn.addEventListener("click", () => {
-    console.log("clicked");
+    const tl = gsap.timeline();
+
+    tl.to(btn, {
+      opacity: 0,
+      y: -40,
+      ease: "expo.inOut",
+      duration: 1,
+    })
+      .to(
+        text,
+        {
+          x: 500,
+          duration: 2,
+          stagger: 0.05,
+          ease: "expo.inOut",
+        },
+        "-=1.1"
+      )
+      .to(
+        ".text-wrapper",
+        {
+          y: -600,
+          scale: 3.5,
+          rotate: -90,
+          ease: "expo.inOut",
+          duration: 2,
+        },
+        "-=1.5"
+      )
+      .to(
+        ".text h1",
+        {
+          opacity: 1,
+          ease: "expo.inOut",
+          duration: 1,
+        },
+        "-=1"
+      )
+      .to(text, {
+        x: -1500,
+        duration: 2,
+        ease: "expo.inOut",
+        stagger: 0.05,
+      })
+      .to(
+        ".header",
+        {
+          y: "0%",
+          ease: "expo.inOut",
+          duration: 1.5,
+        },
+        "-=1.8"
+      );
   });
 });
