e90807242913264245cf9618faaaa24cc2852f7f
prithiviPM <prithivipemimagar@gmail.com>
2026-01-14 11:28:34 +0545

    feat: Implement reveal marquee animation with GSAP timeline for enhanced visual effects

--- reveal-marquee-animation/script.js
+++ reveal-marquee-animation/script.js
@@ -1,3 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-})
+  const tl = gsap.timeline({
+    defaults: { ease: "power2.inOut", duration: 3.5 },
+  });
+
+  tl.from(".clip-top , .clip-bottom", {
+    duration: 2,
+    delay: 0.2,
+    height: "50vh",
+  })
+    .to(
+      ".marquee",
+      {
+        duration: 2,
+        top: "50%",
+      },
+      "-=1.5"
+    )
+    .from(
+      ".clip-top .marquee, .clip-bottom .marquee",
+      {
+        duration: 5,
+        left: "100%",
+        ease: "power3.inOut",
+      },
+      "-=2.5"
+    )
+    .from(
+      ".clip-center .marquee",
+      {
+        duration: 5,
+        left: "-50%",
+        ease: "power3.inOut",
+      },
+      "<"
+    )
+    .to(
+      ".clip-top",
+      {
+        duration: 1,
+        clipPath: "inset(0 0 100% 0)",
+      },
+      "-=1"
+    )
+    .to(
+      ".clip-bottom",
+      {
+        duration: 1,
+        clipPath: "inset(100% 0 0 0)",
+      },
+      "<"
+    )
+    .to(
+      "clip-top .marquee, .clip-bottom .marquee, .clip-center .marquee span",
+      {
+        duration: 1,
+        opacity: 0,
+        ease: "power2.inOut",
+      },"-=.9.8"
+    );
+});
