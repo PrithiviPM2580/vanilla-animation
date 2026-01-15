e39dbd51941c950d974444ab412826d9bcc1a40e
prithiviPM <prithivipemimagar@gmail.com>
2026-01-15 19:40:27 +0545

    feat: Update text animation effects and improve eye movement responsiveness

--- mousemove-eye-animation/script.js
+++ mousemove-eye-animation/script.js
@@ -21,7 +21,57 @@ document.addEventListener("DOMContentLoaded", () => {
 
   elements.forEach((el) => {
     el.addEventListener("mouseover", (e) => {
-      el.classList.add("play");
+      el.classList.remove("play");
     });
   });
+
+  const svg = document.querySelector("#svg");
+  const mouse = svg.createSVGPoint();
+
+  const leftEye = createEye("#left-eye");
+  const rightEye = createEye("#right-eye");
+
+  let requestId = null;
+
+  window.addEventListener("mousemove", onMouseMove);
+
+  function onFrame() {
+    let point = mouse.matrixTransform(svg.getScreenCTM().inverse());
+
+    leftEye.rotateTo(point);
+    rightEye.rotateTo(point);
+
+    requestId = null;
+  }
+
+  function onMouseMove(e) {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+
+    if (!requestId) {
+      requestId = requestAnimationFrame(onFrame);
+    }
+  }
+
+  function createEye(selector) {
+    const element = document.querySelector(selector);
+
+    element.style.transformOrigin = "center";
+
+    let box = element.getBBox();
+    let cx = box.x + box.width / 2;
+    let cy = box.y + box.height / 2;
+
+    function rotateTo(point) {
+      let dx = point.x - cx;
+      let dy = point.y - cy;
+      let angle = (Math.atan2(dy, dx) * 180) / Math.PI;
+      element.style.transform = `rotate(${angle}deg)`;
+    }
+
+    return {
+      element,
+      rotateTo,
+    };
+  }
 });
