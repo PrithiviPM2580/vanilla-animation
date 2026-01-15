ee059c41c475b20137427cfb03556aa9f4362a73
prithiviPM <prithivipemimagar@gmail.com>
2026-01-15 19:21:37 +0545

    feat: Implement text animation on mouseover with dynamic letter rendering

--- mousemove-eye-animation/script.js
+++ mousemove-eye-animation/script.js
@@ -1,1 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".text");
+
+  elements.forEach((el) => {
+    let innerText = el.innerText;
+    el.innerHTML = "";
+
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("block");
+
+    for (let letter of innerText) {
+      let span = document.createElement("span");
+      span.innerText = letter.trim() === "" ? "\xa0" : letter;
+      span.classList.add("letter");
+      textContainer.appendChild(span);
+    }
+
+    el.appendChild(textContainer);
+    el.appendChild(textContainer.cloneNode(true));
+  });
+
+  elements.forEach((el) => {
+    el.addEventListener("mouseover", (e) => {
+      el.classList.add("play");
+    });
+  });
+});
