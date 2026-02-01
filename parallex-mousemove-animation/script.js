373b95dbc4064cbeb77c8941b30f9e97644cc1af
prithiviPM <prithivipemimagar@gmail.com>
2026-02-01 19:29:26 +0545

    feat: Add parallax mousemove animation with HTML, CSS, and JavaScript implementation

---
+++ parallex-mousemove-animation/script.js
@@ -0,0 +1,85 @@
+const items = [
+  {
+    img: "../images/img1.png",
+    parallexSpeed: 0.04,
+  },
+  {
+    img: "../images/img2.png",
+    parallexSpeed: 0.03,
+  },
+  {
+    img: "../images/img3.png",
+    parallexSpeed: 0.06,
+  },
+  {
+    img: "../images/img4.png",
+    parallexSpeed: 0.08,
+  },
+  {
+    img: "../images/img5.png",
+    parallexSpeed: 0.1,
+  },
+  {
+    img: "../images/img6.png",
+    parallexSpeed: 0.05,
+  },
+  {
+    img: "../images/img1.png",
+    parallexSpeed: 0.04,
+  },
+  {
+    img: "../images/img2.png",
+    parallexSpeed: 0.03,
+  },
+  {
+    img: "../images/img3.png",
+    parallexSpeed: 0.06,
+  },
+  {
+    img: "../images/img4.png",
+    parallexSpeed: 0.08,
+  },
+];
+
+const positions = [
+  {
+    top: "23%",
+    left: "67%",
+  },
+  {
+    top: "12%",
+    left: "88%",
+  },
+  {
+    top: "45%",
+    left: "34%",
+  },
+  {
+    top: "78%",
+    left: "21%",
+  },
+  {
+    top: "56%",
+    left: "72%",
+  },
+  {
+    top: "34%",
+    left: "89%",
+  },
+  {
+    top: "67%",
+    left: "15%",
+  },
+  {
+    top: "89%",
+    left: "46%",
+  },
+  {
+    top: "12%",
+    left: "78%",
+  },
+  {
+    top: "50%",
+    left: "50%",
+  },
+];
